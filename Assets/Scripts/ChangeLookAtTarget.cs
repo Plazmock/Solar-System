1f30a1bf4c227e881b1eebe079926c7b3fdd113b
samir-github <Samir>
2016-02-06 15:51:33 +0200

    Version1

---
+++ Assets/Scripts/ChangeLookAtTarget.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChangeLookAtTarget : MonoBehaviour {
+
+	public GameObject target; // the target that the camera should look at
+    public Vector3 CameraDisplacement = new Vector3(0, 0, -3);
+    void Start() {
+		if (target == null) 
+		{
+			target = this.gameObject;
+			Debug.Log ("ChangeLookAtTarget target not specified. Defaulting to parent GameObject");
+		}
+	}
+
+	// Called when MouseDown on this gameObject
+	void OnMouseDown () {
+		// change the target of the LookAtTarget script to be this gameobject.
+		LookAtTarget.target = target;
+        //Camera.main.fieldOfView = 60*target.transform.localScale.x;
+        Camera.main.transform.position = target.transform.position + CameraDisplacement * target.transform.localScale.x;
+    }
+}
---
+++ Assets/Scripts/LookAtTarget.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LookAtTarget : MonoBehaviour {
+
+	static public GameObject target; // the target that the camera should look at
+    public int speed = 2;
+	void Start () {
+		if (target == null) 
+		{
+			target = this.gameObject;
+			Debug.Log ("LookAtTarget target not specified. Defaulting to parent GameObject");
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (target)
+        {
+			transform.LookAt(target.transform);
+           // transform.position = target.transform.position + CameraDisplacement;
+            transform.parent = target.transform;
+            if (target != this.gameObject)
+                transform.RotateAround(target.transform.position, target.transform.up, speed * Time.deltaTime);
+        }
+	}
+}
